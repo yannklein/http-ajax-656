1313cb6e4433e26d8e49633dd53e1cd7fcd9be46
Yann Klein <yann.klein@me.com>
2021-08-04 11:05:39 +0900

    build done

--- dist/main.js
+++ dist/main.js
@@ -96,68 +96,60 @@
 // //////////////////////
 // Rehearsal
 // //////////////////////
-
-// // 1. Select the button 
+// 1. Select the button
 // const button = document.querySelector("#click-me");
-
 // // 2. Listen to a click
 // button.addEventListener("click", (event) => {
-//   // 3. Change the DOM, add disabled and new content
-//   event.currentTarget.classList.add("disabled");
+//   console.log(event);
+//   // 3. Change the DOM, innerText, class disabled
+//   // event.currentTarget.classList.add("disabled");
+//   event.currentTarget.disabled = true;
 //   event.currentTarget.innerText = "Loading...";
 // });
-
 // //////////////////////
 // HTTP GET request
 // //////////////////////
+// 1. Select the input, button, list
+var input = document.querySelector("#keyword");
+var search = document.querySelector("#submit");
+var results = document.querySelector("#results"); // 2. Listen to a click on the button
 
-
-// 1. Select elements (ul, search button, input)
-const input = document.querySelector("#keyword");
-const button = document.querySelector("#submit");
-const results = document.querySelector("#results");
-
-// 2. Listen to a click on the button
-button.addEventListener("click", (event) => {
+search.addEventListener("click", function (event) {
   event.preventDefault();
-  const url = `http://www.omdbapi.com/?s=${input.value}&apikey=adf1f2d7`
-  // console.log(event);
-  // 2.5 Fetch the OMDB API
-  fetch(url)
-    .then(response => response.json())
-    .then((data) => {
-      // console.log(data.Search);
-      // 3. Display the movies
-      results.innerHTML = "";
-      data.Search.forEach((movie) => {
-        results.insertAdjacentHTML(
-          "beforeend",
-          `<li class='list-inline-item'>
-            <img src="${movie.Poster}" alt="poster" />
-            <p>${movie.Title}</p>
-          </li>`);
-      });
-    })
-});
+  console.log(event); // 2.5 Fetch the OMDBAPI
 
+  var url = "https://www.omdbapi.com/?s=".concat(input.value, "&apikey=adf1f2d7");
+  fetch(url).then(function (response) {
+    return response.json();
+  }).then(function (data) {
+    console.log(data);
+    var movies = data.Search; // 3. Change the DOM, display the movies
 
-// //////////////////////
+    results.innerHTML = "";
+    movies.forEach(function (movie) {
+      results.insertAdjacentHTML("beforeend", "<li class='list-inline-item'>\n          <img src=\"".concat(movie.Poster, "\" alt=\"poster\" />\n          <p>").concat(movie.Title, "</p>\n        </li>"));
+    });
+  });
+}); // //////////////////////
 // HTTP POST request
 // //////////////////////
 
-const searchAlgoliaPlaces = (event) => {
+var searchAlgoliaPlaces = function searchAlgoliaPlaces(event) {
+  var someData = {
+    query: event.currentTarget.value
+  };
   fetch("https://places-dsn.algolia.net/1/places/query", {
     method: "POST",
-    body: JSON.stringify({ query: event.currentTarget.value })
-  })
-    .then(response => response.json())
-    .then((data) => {
-      console.log(data.hits); // Look at local_names.default
-    });
+    body: JSON.stringify(someData)
+  }).then(function (response) {
+    return response.json();
+  }).then(function (data) {
+    console.log(data.hits); // Look at local_names.default
+  });
 };
 
-const search = document.querySelector("#search");
-search.addEventListener("keyup", searchAlgoliaPlaces);
+var searchInput = document.querySelector("#search");
+searchInput.addEventListener("keyup", searchAlgoliaPlaces);
 
 /***/ })
 
